978958a990aa1f62d15babf583d25b16944c86f0
Gabriel Kristian <gkqsj@cin.ufpe.br>
2019-11-23 08:48:12 -0300

    feature/navBar:Adição de background para testes e ícones corretos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
 import './App.css';
 import Contato from './components/Contato/Contato'
 import Servicos from './components/Servicos/Servicos'
