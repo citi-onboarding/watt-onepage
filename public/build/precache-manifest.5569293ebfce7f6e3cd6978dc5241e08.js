9bccb18cefc2885b74f6ef2d36a3663701bb8a3d
Gabriel Kristian <gkqsj@cin.ufpe.br>
2019-11-24 14:09:07 -0300

    mongo-Atlas-Intergration: MongoDB usando Atlas

---
+++ public/build/precache-manifest.5569293ebfce7f6e3cd6978dc5241e08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c4c10f1cc275c84fb2e34f9f14d8774",
+    "url": "/index.html"
+  },
+  {
+    "revision": "978b69b6140421dc35e3",
+    "url": "/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "b386c8d297898483daea",
+    "url": "/static/js/2.59bacf1b.chunk.js"
+  },
+  {
+    "revision": "978b69b6140421dc35e3",
+    "url": "/static/js/main.210ef47e.chunk.js"
+  },
+  {
+    "revision": "9aabb23bcbd2c9e69d36",
+    "url": "/static/js/runtime-main.baeb1881.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
