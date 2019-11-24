d78a50dc4afa827772a141ee9eac3da2895b1019
Gabriel Kristian <gkqsj@cin.ufpe.br>
2019-11-24 10:55:58 -0300

    feature/navBar: Componentização da navBar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
-import Navbar from 'react-bootstrap/Navbar'
+import React, {Component} from 'react';
+import MyNavBar from './components/MyNavBar/MyNavBar';
+import background from './imagens/hossein-soltanloo-2ounD6ulngI-unsplash.jpg';
 import './App.css';
 import Contato from './components/Contato/Contato'
 import Servicos from './components/Servicos/Servicos'
@@ -9,20 +10,29 @@ class App extends Component {
   constructor(props) {
     super(props)
 
-    this.state = {
 
-    }
-  }
-
-  render() {
+  render(){
     return (
-      <>
-        <Banner />
-        <Servicos />
-        <Contato />
-        <Footer />
-      </>
-    )
+      <div className="App" style={{
+        backgroundImage: `url(${background})`,
+        backgroundSize:"100%",
+        backgroundRepeat: "no-repeat"
+        }} >
+
+        <MyNavBar />
+
+        <div className = "content">  
+          <a id="Servicos">Servicos</a>
+          <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+          
+          <a id="Sobre">Sobre</a>
+          <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+          
+          <a id="Contato">Contato</a>
+          <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+        </div>
+      </div>
+    );
   }
 }
 export default App;
---
+++ frontend/src/components/MyNavBar/MyNavBar.jsx
@@ -0,0 +1,82 @@
+import React,  {Component} from 'react';
+import './MyNavBar.css';
+import face from '../../ícones/facebook.png';
+import linked from '../../ícones/linkedin-(1).png';
+import insta from '../../ícones/instagram-(2).png';
+import WATTLogo from '../../WATTBRANCO.png';
+
+class MyNavBar extends Component{
+    constructor(props){
+        super(props);
+    }
+
+      
+  handleClickServicos(){
+
+    let elmnt = document.getElementById("Servicos");
+    elmnt.scrollIntoView({
+      behavior: "smooth",
+    });
+  }
+  handleClickSobre(){
+
+    let elmnt = document.getElementById("Sobre");
+    elmnt.scrollIntoView({
+      behavior: "smooth",
+    });
+  }
+  handleClickContato(){
+
+    let elmnt = document.getElementById("Contato");
+    elmnt.scrollIntoView({
+      behavior: "smooth",
+    });
+  }
+  
+
+    render(){
+        return(
+        <div id="navBar" className="navBar" >
+          <div id="logo" className="firstLogo" >
+            <img src={WATTLogo} alt="WATT Consusltoria" height="60" width="60"></img>
+          </div>
+            <ul>
+              
+              <li><a onClick={()=> this.handleClickServicos()} >SERVIÇOS</a></li>
+              <li><a onClick={()=> this.handleClickSobre()} >SOBRE</a></li>
+              <li><a onClick={()=> this.handleClickContato()} >CONTATO</a></li>
+              <li><a target="_blank" href="http://wattconsultoria.com.br/blog/">BLOG</a></li>
+              
+              <li>
+                
+                  <a href="https://www.instagram.com/wattconsultoria/" >
+                    <img className="icons" src={insta} alt="Instagram"></img>
+                  </a>
+                
+              </li>
+                
+              <li>
+                
+                  <a href="https://www.linkedin.com/company/watt-consultoria" >
+                    <img className="icons" src={linked} alt="LinkedIn"></img>
+                  </a>
+                
+              </li>
+                  
+              <li>
+                
+                  <a href="https://www.facebook.com/WattConsultoria/" >
+                    <img className="icons" src={face} alt="Facebook"></img>
+                  </a>
+                
+              </li>
+              
+
+            </ul>
+        </div>
+        )
+    }
+
+}
+
+export default MyNavBar;
