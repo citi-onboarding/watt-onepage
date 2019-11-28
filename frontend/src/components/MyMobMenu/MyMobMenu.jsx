61cef9028ac72279e13ca47cf8543d88cadc3ec0
Gabriel Kristian <gkqsj@cin.ufpe.br>
2019-11-28 01:24:42 -0300

    feature/navBar: Criação do botão trigger para a animação de saída

--- frontend/src/components/MyMobMenu/MyMobMenu.jsx
+++ frontend/src/components/MyMobMenu/MyMobMenu.jsx
@@ -45,38 +45,44 @@ class MyMobMenu extends Component{
     }
   }
 
-    render(){
-        return(
-            <div id="MobMenu" className="mobMenu" >
-                <div id="mobLogo" className="firstMobLogo" >
-                    <a href="/"><img src={WATTLogoVetor} alt="WATT Consusltoria" height="60" width="160"></img></a>
-                </div>
-                
-                <div className = "ulTable" >
-                    
-                    <div className = "liTable" ><a onClick={()=> {this.handleClickServicos();this.animate()}} >SERVIÇOS</a></div>
-                    <div className = "liTable" ><a onClick={()=> {this.handleClickSobre();this.animate()}} >SOBRE</a></div>
-                    <div className = "liTable" ><a onClick={()=> {this.handleClickContato();this.animate()}} >CONTATO</a></div>
-                    <div className = "liTable" ><a target="_blank" href="http://wattconsultoria.com.br/blog/">BLOG</a></div>
-                    
-                </div>
-                
-                <div className="iconTable" >
-                    <a href="https://www.instagram.com/wattconsultoria/" >
-                        <img className="mobIcons" src={insta} alt="Instagram"></img>
-                    </a>
-                
-                    <a href="https://www.linkedin.com/company/watt-consultoria" >
-                        <img className="mobIcons" src={linked} alt="LinkedIn"></img>
-                    </a>
-                
-                    <a href="https://www.facebook.com/WattConsultoria/" >
-                        <img className="mobIcons" src={face} alt="Facebook"></img>
-                    </a>
-                </div>
-            </div>
-        )
-    }
+  render(){
+    return(
+      <div>
+        <div className="buttonWrapper" >
+          <a className="fixedMobButton" ><img onClick={()=> this.animate()} color="#FFFFFF" src={menuIcon} alt="WATT Consusltoria" height="60" width="60"></img></a>  
+        </div>
+        <div id="MobMenu" className="mobMenu" >     
+
+          <div id="mobLogo" className="firstMobLogo" >
+            <a href="/"><img src={WATTLogoVetor} alt="WATT Consusltoria" height="60" width="160"></img></a>
+          </div>
+          
+          <div className = "ulTable" >
+              
+            <div className = "liTable" ><a onClick={()=> {this.handleClickServicos();this.animate()}} >SERVIÇOS</a></div>
+            <div className = "liTable" ><a onClick={()=> {this.handleClickSobre();this.animate()}} >SOBRE</a></div>
+            <div className = "liTable" ><a onClick={()=> {this.handleClickContato();this.animate()}} >CONTATO</a></div>
+            <div className = "liTable" ><a target="_blank" href="http://wattconsultoria.com.br/blog/">BLOG</a></div>
+            
+          </div>
+          
+          <div className="iconTable" >
+            <a href="https://www.instagram.com/wattconsultoria/" >
+              <img className="mobIcons" src={insta} alt="Instagram"></img>
+            </a>
+        
+            <a href="https://www.linkedin.com/company/watt-consultoria" >
+              <img className="mobIcons" src={linked} alt="LinkedIn"></img>
+            </a>
+        
+            <a href="https://www.facebook.com/WattConsultoria/" >
+              <img className="mobIcons" src={face} alt="Facebook"></img>
+            </a>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default MyMobMenu;
